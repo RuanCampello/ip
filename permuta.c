07ab1872caa532207867aaf08784e974ad90f1b3
RuanCampello <ruancampello888@gmail.com>
2024-12-16 17:04:55 -0300

    update permuta

--- permuta.c
+++ permuta.c
@@ -1,13 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void) {
     int a, b;
 
+    /* a entrada de dados nao pode ser alterada */
     printf("Digite o valor de a: ");
     scanf("%d", &a);
     printf("Digite o valor de b: ");
     scanf("%d", &b);
+    /* fim entrada de dados */
 
+    /* esse printf nao pode ser alterado nem removido. */
     printf("\nOs valores lidos para a e b foram: %d e %d\n", a, b);
 
     // isso usa "ou exclusivo" (XOR) em operações binárias, mas pode ser perigoso.
@@ -19,6 +22,7 @@ int main(void) {
     b = a - b;
     a = a - b;
 
+    /* esse printf nao pode ser alterado nem removido e nada pode ser alterado apos ele. */
     printf("\nOs novos valores de a e b sao: %d e %d\n", a, b);
 
     return 0;
